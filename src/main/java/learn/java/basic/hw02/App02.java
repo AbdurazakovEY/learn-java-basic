44b0605c948b7d3949d257b2f6c3aebb2a16c534
AbdurazakovEY <AbdurazakovEY@gmail.com>
2024-10-10 17:14:13 +0300

    ДЗ02 выполнено.

--- src/main/java/learn/java/basic/hw02/App02.java
+++ src/main/java/learn/java/basic/hw02/App02.java
@@ -3,6 +3,20 @@ package learn.java.basic.hw02;
 public class App02 {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        printOneTwoThree();
+        printOneTwo();
+    }
+
+    public static void printOneTwoThree() {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+
+    }
+
+    public static void printOneTwo() {
+        System.out.println(1);
+        System.out.println(2);
     }
 }
 
