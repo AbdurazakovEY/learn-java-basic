950c3ddb8d0b91e8e3c642c2990939a458472004
AbdurazakovEY <AbdurazakovEY@gmail.com>
2025-01-22 14:10:03 +0300

    ДЗ07 выполнено частично v3.

--- src/main/java/learn/java/basic/hw07/App07.java
+++ src/main/java/learn/java/basic/hw07/App07.java
@@ -91,9 +91,12 @@ public class App07 {
     */
     public static int sumOfElementsTheSecondRow2dArray(int[][] arr) {
         int sum = 0;
-            for (int i = 1; i < arr.length-1; i++) {
+            for (int i = 1; i < arr.length-1; ++i) {
+                if (arr.length < 2) {
+                    return -1;
+                }
                 for (int j = 0; j < arr[i].length; j++) {
-                        sum += arr[i][j];
+                    sum += arr[i][j];
                 }
             }
         return sum;
