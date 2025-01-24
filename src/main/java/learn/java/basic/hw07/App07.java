32d9187667ee4ba151a9f990810360adb1980959
AbdurazakovEY <AbdurazakovEY@gmail.com>
2025-01-24 13:25:49 +0300

    ДЗ07 выполнено частично v5.

--- src/main/java/learn/java/basic/hw07/App07.java
+++ src/main/java/learn/java/basic/hw07/App07.java
@@ -9,18 +9,18 @@ public class App07 {
                 {4, 5, 6},
                 {7, 8, 9}
         };
-        int size = 3;
 
         int sum = sumOfPositiveElements(arr2d);
         out.println(sum);
         out.println();
-        printSquareOfSymbols(size);
+        printSquareOfSymbols();
         zeroingDiagonalElements(arr2d);
         int max = findMax(arr2d);
         out.println(max);
         out.println();
         int sumOfElements = sumOfElementsTheSecondRow2dArray(arr2d);
         out.println(sumOfElements);
+
     }
 
     /**
@@ -30,9 +30,11 @@ public class App07 {
      */
     public static int sumOfPositiveElements(int[][] arr) {
         int sum = 0;
-        for (int[] arrLine : arr) {
-            for (int anInt : arrLine) {
-                sum += anInt;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if(arr[i][j] > 0) {
+                    sum += arr[i][j];
+                }
             }
         }
         return sum;
@@ -42,13 +44,10 @@ public class App07 {
      * 2. Реализовать метод, который принимает в качестве аргумента int size
      * и печатает в консоль квадрат из символов * со сторонами соответствующей длины;
      */
-    public static void printSquareOfSymbols(int size) {
-        char[][] map;
-        map = new char[size][size];
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[i].length; j++) {
-                map[i][j] = '*';
-                out.print(map[i][j] + " ");
+    public static void printSquareOfSymbols() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                out.print("* ");
             }
             out.println();
         }
@@ -90,17 +89,13 @@ public class App07 {
     * если второй строки не существует, то в качестве результата необходимо вернуть -1
     */
     public static int sumOfElementsTheSecondRow2dArray(int[][] arr) {
+        if (arr.length < 2) {
+            return -1;
+        }
         int sum = 0;
-            for (int i = 1; i < arr.length-1; ++i) {
-                if (arr.length < 2) {
-                    return -1;
-                }
-                else {
-                    for (int j = 0; j < arr[i].length; j++) {
-                        sum += arr[i][j];
-                    }
-                }
-            }
+        for (int i = 0; i < arr[1].length; i++) {
+            sum += arr[1][i];
+        }
         return sum;
     }
 }
